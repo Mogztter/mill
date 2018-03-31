45cbb2eeb252c842bca7fb4b0e4db11612f16349
Robby <robby@k-state.edu>
2018-03-31 07:15:08 -0500

    Enhanced test-mill-release.sh to do self build test.

--- ci/test-mill-release.sh
+++ ci/test-mill-release.sh
@@ -5,12 +5,30 @@ set -eux
 # Starting from scratch...
 git clean -xdf
 
-ci/publish-local.sh
+# First build
 
-git clean -xdf
+mill -i all __.publishLocal release
 
-rm -fR ~/.mill
+mv out/release/dest/mill ~/mill-1
 
-# Second build & run tests using Mill
+# Clean up
 
-~/mill-release -i integration.test "mill.integration.forked.{AcyclicTests,UpickleTests,PlayJsonTests}"
+git clean -xdf && rm -fR ~/.mill
+
+# Differentiate first and second builds
+
+echo "Build 2" > info.txt && git add info.txt && git commit -m "Add info.txt"
+
+# Second build
+
+~/mill-1 -i all __.publishLocal release
+
+mv out/release/dest/mill ~/mill-2
+
+# Clean up
+
+git clean -xdf && rm -fR ~/.mill
+
+# Use second build to run tests using Mill
+
+~/mill-2 -i integration.test "mill.integration.forked.{AcyclicTests,UpickleTests,PlayJsonTests}"
